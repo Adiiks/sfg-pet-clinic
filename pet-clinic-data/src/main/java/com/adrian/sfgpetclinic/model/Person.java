887bb8e19541db284b28e199e6b9dd34b99cda85
Adiks <apacholak97@interia.pl>
2022-05-19 09:21:49 +0200

    implemented base entity

--- pet-clinic-data/src/main/java/com/adrian/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/adrian/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.adrian.sfgpetclinic.model;
 
-public class Person extends BaseEntity {
+public abstract class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
