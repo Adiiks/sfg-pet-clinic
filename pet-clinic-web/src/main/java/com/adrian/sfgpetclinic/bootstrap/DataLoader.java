6dd9c789b86667b656351c52ce26b93dac833550
Adiks <apacholak97@interia.pl>
2022-05-20 09:19:29 +0200

    added spring data jpa service for visit and initial data on startup

--- pet-clinic-web/src/main/java/com/adrian/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/adrian/sfgpetclinic/bootstrap/DataLoader.java
@@ -5,6 +5,7 @@ import com.adrian.sfgpetclinic.services.OwnerService;
 import com.adrian.sfgpetclinic.services.PetTypeService;
 import com.adrian.sfgpetclinic.services.SpecialityService;
 import com.adrian.sfgpetclinic.services.VetService;
+import com.adrian.sfgpetclinic.services.springdatajpa.VisitService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +18,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialityService specialityService) {
+                      SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -85,6 +88,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasPet);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Snezzy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded owners ...");
 
