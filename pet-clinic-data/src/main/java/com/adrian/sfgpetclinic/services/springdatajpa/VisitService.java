984d6742d6cd4ba7bf9c2e581ce42c0de6a8a319
Adiks <apacholak97@interia.pl>
2022-05-20 09:08:50 +0200

    added map service for visit

---
+++ pet-clinic-data/src/main/java/com/adrian/sfgpetclinic/services/springdatajpa/VisitService.java
@@ -0,0 +1,7 @@
+package com.adrian.sfgpetclinic.services.springdatajpa;
+
+import com.adrian.sfgpetclinic.model.Visit;
+import com.adrian.sfgpetclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
